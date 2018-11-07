91d4c82d38b0d0861c4febb0b858d4b747a72cbd
Mya Stewart <stewarmy@oregonstate.edu>
2018-11-06 16:47:31 -0800

    js maintenance

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
 $(updateView)
 
 function updateView() {
-    $.getJSON(BASE_URL+ "/rides/count" , updateRideCount)
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 }
 
 function updateRideCount(data) {
-    numberOfRides = data.count
-    $("#h2rideCount").html(numberOfRides)
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
