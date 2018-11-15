e15a64db2e031db8a267a5b6f33005e1287f3aeb
Mya Stewart <stewarmy@oregonstate.edu>
2018-11-15 12:05:40 -0800

    Modify html and js for data and chart

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
+var theRealData
+
 $(updateView)
 
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+  $.getJSON(BASE_URL + "/rides/count/per_hour" , updateRideHour)
 }
 
 function updateRideCount(data) {
   numberOfRides = data.count
   $("h2#rideCount").html(numberOfRides)
 }
+
+function updateRideHour(data) {
+  console.log(data)   
+}
+
