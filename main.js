3513c7a6039e3608dcc8c3f3d32fdf63b66a1203
Mya Stewart <stewarmy@oregonstate.edu>
2018-11-27 21:07:10 -0800

    Modify Chart and js to include real data

--- main.js
+++ main.js
@@ -1,20 +1,52 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-var theRealData
 
 $(updateView)
 
 function updateView() {
-  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
-  $.getJSON(BASE_URL + "/rides/count/per_hour" , updateRideHour)
+ displayGraph
 }
 
-function updateRideCount(data) {
-  numberOfRides = data.count
-  $("h2#rideCount").html(numberOfRides)
+function displayGraph(data) {
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+
 }
 
-function updateRideHour(data) {
-  console.log(data)   
-}
 
